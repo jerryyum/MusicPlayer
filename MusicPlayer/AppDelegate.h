b273197e604897ad2311355528e9f20b84c8893d
jerryyum <jerryyum@qq.com>
2019-10-12 10:40:29 +0800

    Initial Commit

---
+++ MusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by miao yu on 2019/10/12.
+//  Copyright © 2019 jerryyum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicPlayer
+//
+//  Created by miao yu on 2019/10/12.
+//  Copyright © 2019 jerryyum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
