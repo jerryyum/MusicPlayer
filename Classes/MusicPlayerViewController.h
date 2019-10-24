679b1dc3405d862e72d5fc04b6cfeb5632ff07ad
jerryyum <jerryyum@qq.com>
2019-10-24 16:51:48 +0800

    add resources

---
+++ Classes/MusicPlayerViewController.h
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerViewController.h
+//  MusicPlayer
+//
+//  Created by miao yu on 2019/10/24.
+//  Copyright © 2019 jerryyum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MusicPlayerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/MusicPlayerViewController.m
@@ -0,0 +1,24 @@
+//
+//  MusicPlayerViewController.m
+//  MusicPlayer
+//
+//  Created by miao yu on 2019/10/24.
+//  Copyright © 2019 jerryyum. All rights reserved.
+//
+
+#import "MusicPlayerViewController.h"
+
+@interface MusicPlayerViewController ()
+
+@end
+
+@implementation MusicPlayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+@end
