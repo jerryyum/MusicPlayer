547e2243b8da07208a745dd90066e8f7b8de9c0e
jerryyum <jerryyum@qq.com>
2019-11-05 12:10:03 +0800

    cc

--- Classes/JYMusicPlayer.h
+++ Classes/JYMusicPlayer.h
@@ -8,9 +8,53 @@
 
 #import <Foundation/Foundation.h>
 
+#define INVALID_PLAYING_INDEX  -1 // 无效的播放索引
+
 NS_ASSUME_NONNULL_BEGIN
 
+/// 歌曲播放循环模式
+typedef NS_ENUM(NSInteger, PlayerLoopMode) {
+    PlayerLoopAll        = 0, /// 列表循环
+    PlayerLoopShuffle    = 1, /// 随机播放
+    PlayerLoopSingle     = 2, /// 单曲循环
+};
+
+
+/// 音乐播放器类，负责音乐播放、暂停、切换等
 @interface JYMusicPlayer : NSObject
+
+/// 当前的歌曲列表
+@property (nonatomic, strong, readonly) NSArray<NSString *> *songs;
+
+/// 正在播放的歌曲索引
+@property (nonatomic, assign, readonly) NSInteger playingIdx;
+
+/// 歌曲播放循环模式
+@property (nonatomic, assign, readonly) PlayerLoopMode loopMode;
+
+#pragma mark - init
+
+- (instancetype)init;
+
+#pragma mark - Add Song
+
+/// 添加一首歌曲
+- (void)addSong:(NSString *)song;
+
+
+/// 添加多首歌曲
+/// @param songs 要添加的歌曲
+- (void)addSongs:(NSArray<NSString *> *)songs;
+
+#pragma mark - Player Operations
+
+- (void)play;
+- (void)pause;
+- (void)playPreSong;
+- (void)playNextSong;
+- (void)playIndexSong:(NSInteger)index;
+
+- (void)changeLoopMode;
 
 @end
 
--- Classes/JYMusicPlayer.m
+++ Classes/JYMusicPlayer.m
@@ -8,6 +8,164 @@
 
 #import "JYMusicPlayer.h"
 
+#import <AVFoundation/AVFoundation.h>
+
+@interface JYMusicPlayer ()
+
+/// 当前的歌曲列表
+@property (nonatomic, strong) NSMutableArray<NSString *> *songs;
+/// 正在播放的歌曲索引
+@property (nonatomic, assign) NSInteger playingIdx;
+/// 歌曲播放循环模式
+@property (nonatomic, assign) PlayerLoopMode loopMode;
+
+/// 当前歌曲的播放器对象，切换歌曲时需要重新创建该对象
+@property (nonatomic, strong) AVAudioPlayer *audioPlayer;
+
+@end
+
 @implementation JYMusicPlayer
+
+- (NSArray<NSString *> *)songs {
+    return [_songs copy];
+}
+
+/// 根据循环模式，产生下一个索引
+- (NSInteger)_nextIndex {
+    if (_songs == nil || _songs.count == 0) {
+        return INVALID_PLAYING_INDEX;
+    }
+    
+    NSInteger nextIndex = INVALID_PLAYING_INDEX;
+    if (_loopMode == PlayerLoopAll) {
+        nextIndex = (_playingIdx + 1) % _songs.count;
+    } else if (_loopMode == PlayerLoopSingle) {
+        nextIndex = _playingIdx == INVALID_PLAYING_INDEX ? 0 : _playingIdx;
+    } else if (_loopMode == PlayerLoopShuffle) {
+        NSUInteger songCount = _songs.count;
+        nextIndex = arc4random_uniform((u_int32_t)songCount); // 产生随机数
+        if (nextIndex == _playingIdx) {
+            // 产生的随机数与前播放歌曲序号相等, 则+1确保不要播同一首歌
+            nextIndex = (nextIndex + 1) % songCount;
+        }
+    }
+    return nextIndex;
+}
+
+#pragma mark - init
+
+- (instancetype)init {
+    self = [super init];
+    _playingIdx = INVALID_PLAYING_INDEX;
+    _loopMode = PlayerLoopAll;
+    return self;
+}
+
+#pragma mark - Add Song
+
+- (void)addSong:(NSString *)song {
+    if (_songs != nil) {
+        [_songs addObject:song];
+    } else {
+        _songs = [NSMutableArray arrayWithObjects:song, nil];
+    }
+}
+
+- (void)addSongs:(NSArray<NSString *> *)songs {
+    if (_songs != nil) {
+        [_songs addObjectsFromArray:songs];
+    } else {
+        _songs = [songs mutableCopy];
+    }
+}
+
+#pragma mark - Player Operations
+
+- (void)play {
+    if (_songs == nil || _songs.count == 0) {
+        NSLog(@"歌曲列表为空，请添加歌曲！");
+        return;
+    }
+    
+    NSInteger index;
+    if (_playingIdx != INVALID_PLAYING_INDEX) {
+        index = _playingIdx;
+    } else {
+        index = [self _nextIndex];
+    }
+    [self playIndexSong:index];
+}
+
+- (void)pause {
+    if (_audioPlayer.isPlaying) {
+        [_audioPlayer pause];
+    }
+}
+
+- (void)playPreSong {
+    if (_songs == nil || _songs.count == 0) {
+        NSLog(@"歌曲列表为空，请添加歌曲！");
+        return;
+    }
+    
+    NSInteger songIdx = 0;
+    if (_playingIdx == 0) {
+        NSLog(@"已经是第一首歌曲！");
+        return;
+        
+    } else if (_playingIdx > 0) {
+        songIdx = _playingIdx - 1;
+    }
+    
+    [self playIndexSong:songIdx];
+}
+
+- (void)playNextSong {
+    if (_songs == nil || _songs.count == 0) {
+        NSLog(@"歌曲列表为空，请添加歌曲！");
+        return;
+    }
+    
+    NSInteger nextIndex = [self _nextIndex];
+    [self playIndexSong:nextIndex];
+}
+
+- (void)playIndexSong:(NSInteger)index {
+    NSAssert(index != INVALID_PLAYING_INDEX, @"无效的歌曲索引");
+    NSAssert(index < _songs.count, @"索引值超过歌曲数量");
+    
+    if (index == _playingIdx) {
+        if (!_audioPlayer.isPlaying) {
+            [_audioPlayer play];
+        }
+    } else {
+        _playingIdx = index;
+        
+        NSString *song = _songs[_playingIdx];
+        NSURL *songURL = [[NSBundle mainBundle] URLForResource:song withExtension:nil subdirectory:@"Resources"];
+        _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:songURL error:nil];
+        [_audioPlayer prepareToPlay];
+        [_audioPlayer play];
+    }
+}
+
+- (void)changeLoopMode {
+    switch (_loopMode) {
+        case PlayerLoopAll:
+            self.loopMode = PlayerLoopShuffle;
+            NSLog(@"随机播放");
+            break;
+        case PlayerLoopShuffle:
+            self.loopMode = PlayerLoopSingle;
+            NSLog(@"单曲循环");
+            break;
+        case PlayerLoopSingle:
+            self.loopMode = PlayerLoopAll;
+            NSLog(@"列表循环");
+            break;
+        default:
+            break;
+    }
+}
 
 @end
